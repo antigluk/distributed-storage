8df0551992f5c0afbbb2b833ecd83a1d1f683e1a
Roman Rader <antigluk@gmail.com>
2013-02-10 20:49:18 +0200

    req

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
 import tornado.web
-import json
+
+import os
+import urllib2
 
 
 class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
-        self.write(json.dumps({"info": "Hello from flask."}))
+        address = os.environ['OPENSHIFT_INTERNAL_IP']
+        string = urllib2.urlopen("http://%s:15001/" % address)
+        self.write(string)
         self.finish()
