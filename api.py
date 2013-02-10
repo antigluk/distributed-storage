caca47670ded71c7b7f3ce5c552a8dd4e09e8c33
Roman Rader <antigluk@gmail.com>
2013-02-10 20:46:10 +0200

    fixed

--- api.py
+++ api.py
@@ -3,5 +3,7 @@ import json
 
 
 class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
     def get(self):
         self.write(json.dumps({"info": "Hello from flask."}))
+        self.finish()
