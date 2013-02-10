aa2daeaf67938e38e9e841cc08d219775ea3727c
Roman Rader <antigluk@gmail.com>
2013-02-10 20:52:35 +0200

    lg

--- api.py
+++ api.py
@@ -8,6 +8,8 @@ class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         address = os.environ['OPENSHIFT_INTERNAL_IP']
+        print "Loading..."
         string = urllib2.urlopen("http://%s:15001/" % address)
-        self.write(string)
+        print string
+        self.write(unicode(string))
         self.finish()
