c250ed5fea41bcfcb3715ebcc7529abf99004e90
Roman Rader <antigluk@gmail.com>
2013-02-11 01:04:36 +0200

    post request

--- api.py
+++ api.py
@@ -15,7 +15,8 @@ celery.config_from_object('celeryconfig')
 @celery.task
 def process_chunk(path, num, chunk_file, hash):
     address = os.environ['OPENSHIFT_INTERNAL_IP']
-    js = json.loads(urllib2.urlopen("http://%s:15001/chunk/%s" % (address, hash)).read())
+    req = urllib2.Request("http://%s:15001/chunk/%s" % (address, hash), {'1': '1'})
+    js = json.loads(urllib2.urlopen(req).read())
     datadir = os.environ['OPENSHIFT_DATA_DIR']
 
     if not js.get('result') == 'OK':
