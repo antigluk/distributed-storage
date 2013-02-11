d3c15cd4c64493625d0ab9b25c0455453b0bdd73
Roman Rader <antigluk@gmail.com>
2013-02-11 12:35:25 +0200

    classmethods

--- storages/Local.py
+++ storages/Local.py
@@ -13,11 +13,13 @@ class Local(object):
     def name(cls):
         return "local"
 
+    @classmethod
     def store_chunk(chunk_file, hash):
         sh.mkdir('-p', os.path.join(datadir, 'local_storage'))
         sh.mv(chunk_file, os.path.join(datadir, 'local_storage', hash))
         sh.rm('-f', chunk_file)
 
+    @classmethod
     def get_chunk(hash):
         file_name = os.path.join(datadir, 'local_storage', hash)
         if os.path.exists(file_name):
