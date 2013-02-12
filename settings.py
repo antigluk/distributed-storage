4400b32424383ef48e1c832a14ddfd2c98c565e8
Roman Rader <antigluk@gmail.com>
2013-02-13 01:41:10 +0200

    app fixed

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ internal_ip = "127.0.0.1"  # os.environ['OPENSHIFT_INTERNAL_IP']
 tmpdir = "/tmp/"  # os.environ['OPENSHIFT_TMP_DIR']
 
 try:
-    import local_settings
+    from local_settings import *
 except ImportError:
     pass
