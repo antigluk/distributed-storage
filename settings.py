550967238a35c01705a79ec18033010d74f863db
Roman Rader <antigluk@gmail.com>
2013-02-12 19:07:56 +0200

    local settings

--- settings.py
+++ settings.py
@@ -3,3 +3,8 @@ import os
 datadir = os.environ['OPENSHIFT_DATA_DIR']  # /var/data/
 internal_ip = os.environ['OPENSHIFT_INTERNAL_IP']  # 127.0.0.1
 tmpdir = os.environ['OPENSHIFT_TMP_DIR']
+
+try:
+	import local_settings
+except ImportError:
+	pass
