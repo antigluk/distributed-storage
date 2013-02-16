2a572a7c5506439674a0c42d8286fcf3f30cc279
Roman Rader <antigluk@gmail.com>
2013-02-16 18:44:12 +0200

    ui

--- ui.py
+++ ui.py
@@ -9,7 +9,7 @@ from ns import nslib
 
 
 class UIHandler(tornado.web.RequestHandler):
-    def get(self, path):
+    def get(self):
         self.write("UI")
         loader = tornado.template.Loader(settings.staticdir)
         self.write(loader.load("fs.html").generate(myvalue="XXX"))
