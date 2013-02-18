f2e4e12ec115452dc030aa3b4ff4224bb960cde2
Roman Rader <antigluk@gmail.com>
2013-02-18 20:07:23 +0200

    upd

--- ns/nslib.py
+++ ns/nslib.py
@@ -192,7 +192,7 @@ def find_server(hash):
         s_list, full_info = scan_stats()
         #>5*chunk MB free space
         min_size = 5 * settings.chunk_size
-        s_list = filter(lambda serv: float(serv['size']) - float(serv['used']) > min_size, s_list)
+        # s_list = filter(lambda serv: (float(serv['size']) - float(serv['used'])) > min_size, s_list)
         if not s_list:
             raise ChunkError("All storages FULL!")
         return sorted(s_list, key=lambda x: x['chunks_count'])[0]['name']
