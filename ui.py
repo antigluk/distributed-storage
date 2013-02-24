d7ed48a88f5cdbb51d954e6bdaa43c3c777b9c46
Roman Rader <antigluk@gmail.com>
2013-02-24 18:49:47 +0200

    upd

--- ui.py
+++ ui.py
@@ -1,10 +1,40 @@
 import tornado.web
 import tornado.template
 
+import base64
+import os
+
 import settings
 from ns import nslib
 
 
+def require_basic_auth(handler_class):
+    def wrap_execute(handler_execute):
+        def require_basic_auth(handler, kwargs):
+            auth_header = handler.request.headers.get('Authorization')
+            if auth_header is None or not auth_header.startswith('Basic '):
+                handler.set_status(401)
+                handler.set_header('WWW-Authenticate', 'Basic realm=Restricted')
+                handler._transforms = []
+                handler.finish()
+                return False
+            auth_decoded = base64.decodestring(auth_header[6:])
+            # kwargs['basicauth_user'], kwargs['basicauth_pass'] = auth_decoded.split(':', 2)
+            user, pwd = auth_decoded.split(':', 2)
+            real_user, real_pwd = os.path.join(settings.datadir, 'pwd').strip().split(":")
+            return (user == real_user) and (real_pwd == pwd)
+
+        def _execute(self, transforms, *args, **kwargs):
+            if not require_basic_auth(self, kwargs):
+                return False
+            return handler_execute(self, transforms, *args, **kwargs)
+        return _execute
+
+    handler_class._execute = wrap_execute(handler_class._execute)
+    return handler_class
+
+
+@require_basic_auth
 class StatsUIHandler(tornado.web.RequestHandler):
     def get(self):
         loader = tornado.template.Loader(settings.staticdir)
@@ -15,6 +45,7 @@ class StatsUIHandler(tornado.web.RequestHandler):
         return
 
 
+@require_basic_auth
 class FSUIHandler(tornado.web.RequestHandler):
     def get(self, path):
         loader = tornado.template.Loader(settings.staticdir)
